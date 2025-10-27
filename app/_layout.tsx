fc2a13b7df1fcd5907144153ebad55771b5b8976
Shashank Maurya <shashankdoeshisstuff@gmail.com>
2025-10-27 21:36:33 +0530

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,46 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
+import { Stack, useRouter } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { AuthProvider } from '@/lib/auth-context';
+import { useEffect } from 'react';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
+
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuth = false;
+
+  useEffect(() => {
+    if (!isAuth) {
+      const timeout = setTimeout(() => {
+        router.replace("/auth");
+      }, 0); // wait one tick for navigation context to be ready
+      return () => clearTimeout(timeout);
+    }
+  }, [isAuth]);
+
+  return <>{children}</>
+}
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+      <AuthProvider>
+        <RouteGuard>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
+          </Stack>
+          <StatusBar style="auto" />
+        </RouteGuard>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
